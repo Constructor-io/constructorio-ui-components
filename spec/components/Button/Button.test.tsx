31cda688e1d8c2d22276a361223184b61cbc9e69
mudaafi <ahmad.mudaafi@constructor.io>
2025-09-19 05:26:13 +0800

    add tests

--- spec/components/Button/Button.test.tsx
+++ spec/components/Button/Button.test.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { render, screen, fireEvent, cleanup } from '@testing-library/react';
 import { describe, test, expect, vi, afterEach } from 'vitest';
-import { Button } from '../../../src/components/Button/Button';
+import Button from '../../../src/components/Button';
 
 describe('Button component', () => {
   afterEach(() => {
     cleanup();
   });
+
   test('renders button with text', () => {
     render(<Button>Click me</Button>);
     expect(screen.getByText('Click me')).toBeInTheDocument();
@@ -23,5 +24,31 @@ describe('Button component', () => {
   test('is disabled when disabled prop is true', () => {
     render(<Button disabled>Click me</Button>);
     expect(screen.getByText('Click me')).toBeDisabled();
+  });
+
+  test('renders component as is when asChild is true', () => {
+    render(
+      <Button asChild>
+        <a>hello</a>
+      </Button>,
+    );
+
+    const renderedButton = screen.getByText('hello');
+    expect(renderedButton.tagName).toBe('A');
+    expect(renderedButton.classList.contains('cio-button')).toBeTruthy();
+  });
+
+  test('renders componentOverride if passed', () => {
+    render(
+      <Button
+        componentOverrides={{ reactNode: <ul className='cio-button-custom'>A random list</ul> }}>
+        <a>hello</a>
+      </Button>,
+    );
+
+    const renderedButton = screen.getByText('A random list');
+    expect(renderedButton.tagName).toBe('UL');
+    expect(renderedButton.classList.contains('cio-button')).toBeFalsy();
+    expect(renderedButton.classList.contains('cio-button-custom')).toBeTruthy();
   });
 });

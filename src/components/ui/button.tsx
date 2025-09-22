acee6d65b98f44ca38304d1ca7a47c5470f6e468
mudaafi <ahmad.mudaafi@constructor.io>
2025-09-23 05:05:38 +0800

    Update styles to match design doc

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,28 +9,35 @@ const buttonVariants = cva(
   "cio-button cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-sm text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
   {
     variants: {
-      variant: {
-        default: 'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
+      mode: {
+        primary: 'bg-primary text-primary-foreground shadow-xs hover:bg-primary/90',
         secondary: 'bg-secondary text-secondary-foreground shadow-xs hover:bg-secondary/80',
         outline:
           'border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50',
         ghost: 'hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50',
+        link: 'text-primary underline-offset-4 hover:underline',
 
         // ShadCDN Defaults
         destructive:
           'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
-        link: 'text-primary underline-offset-4 hover:underline',
       },
       size: {
-        default: 'h-9 px-4 py-2 has-[>svg]:px-3',
-        sm: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
-        lg: 'h-10 rounded-md px-6 has-[>svg]:px-4',
+        sm: 'h-6 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
+        md: 'h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5',
+        lg: 'h-10 px-4 py-2 has-[>svg]:px-3',
+        xl: 'h-12 rounded-md px-6 has-[>svg]:px-4',
         icon: 'size-9',
+      },
+      shape: {
+        beveled: 'rounded-sm',
+        rounded: 'rounded-full',
+        sharp: 'rounded-none',
       },
     },
     defaultVariants: {
-      variant: 'default',
-      size: 'default',
+      mode: 'primary',
+      size: 'lg',
+      shape: 'beveled',
     },
   },
 );
@@ -51,8 +58,9 @@ export interface ButtonProps
 
 export default function Button({
   className,
-  variant,
+  mode,
   size,
+  shape,
   asChild = false,
   componentOverrides,
   ...props
@@ -65,7 +73,7 @@ export default function Button({
     <RenderPropsWrapper props={renderProps} override={componentOverrides?.reactNode}>
       <Comp
         data-slot='button'
-        className={cn(buttonVariants({ variant, size, className }))}
+        className={cn(buttonVariants({ mode, size, shape, className }))}
         {...props}
       />
     </RenderPropsWrapper>
--- src/stories/components/Button/Button.stories.tsx
+++ src/stories/components/Button/Button.stories.tsx
@@ -16,36 +16,115 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    children: 'Button',
+    children: 'Default Button',
     asChild: false,
   },
   tags: ['!autodocs'],
 };
 
+export const DisabledVariant: Story = {
+  args: {
+    children: 'Disabled',
+    disabled: true,
+  },
+};
+
 export const SecondaryVariant: Story = {
   args: {
     children: 'Secondary',
-    variant: 'secondary',
+    mode: 'secondary',
   },
+  name: 'Mode - Secondary',
 };
 
 export const DestructiveVariant: Story = {
   args: {
     children: 'Destructive',
-    variant: 'destructive',
+    mode: 'destructive',
   },
+  name: 'Mode - Destructive',
 };
 
 export const GhostVariant: Story = {
   args: {
     children: 'Ghost',
-    variant: 'ghost',
+    mode: 'ghost',
   },
+  name: 'Mode - Ghost',
 };
 
 export const LinkVariant: Story = {
   args: {
     children: 'Link',
-    variant: 'link',
+    mode: 'link',
   },
+  name: 'Mode - Link',
+};
+
+export const XlVariant: Story = {
+  args: {
+    children: 'Button Size',
+    size: 'xl',
+  },
+  name: 'Size - xl',
+};
+
+export const LgVariant: Story = {
+  args: {
+    children: 'Button Size',
+    size: 'lg',
+  },
+  name: 'Size - lg',
+};
+
+export const MdVariant: Story = {
+  args: {
+    children: 'Button Size',
+    size: 'md',
+  },
+  name: 'Size - md',
+};
+
+export const SmVariant: Story = {
+  args: {
+    children: 'Button Size',
+    size: 'sm',
+  },
+  name: 'Size - sm',
+};
+
+export const IconVariant: Story = {
+  args: {
+    children: (
+      <svg
+        width='25'
+        height='24'
+        viewBox='0 0 25 24'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'>
+        <path
+          d='M12.4199 9.5H14.4199V6.5H17.4199V4.5H14.4199V1.5H12.4199V4.5H9.41992V6.5H12.4199V9.5ZM8.41992 18.5C7.31992 18.5 6.42992 19.4 6.42992 20.5C6.42992 21.6 7.31992 22.5 8.41992 22.5C9.51992 22.5 10.4199 21.6 10.4199 20.5C10.4199 19.4 9.51992 18.5 8.41992 18.5ZM18.4199 18.5C17.3199 18.5 16.4299 19.4 16.4299 20.5C16.4299 21.6 17.3199 22.5 18.4199 22.5C19.5199 22.5 20.4199 21.6 20.4199 20.5C20.4199 19.4 19.5199 18.5 18.4199 18.5ZM9.51992 13.5H16.9699C17.7199 13.5 18.3799 13.09 18.7199 12.47L22.5799 5.46L20.8399 4.5L16.9699 11.5H9.94992L5.68992 2.5H2.41992V4.5H4.41992L8.01992 12.09L6.66992 14.53C5.93992 15.87 6.89992 17.5 8.41992 17.5H20.4199V15.5H8.41992L9.51992 13.5Z'
+          fill='white'
+        />
+      </svg>
+    ),
+    size: 'icon',
+  },
+  name: 'Size - Icon',
+};
+
+export const RoundedVariant: Story = {
+  args: {
+    children: 'Shape - Rounded',
+    shape: 'rounded',
+  },
+  name: 'Shape - Rounded',
+};
+
+export const SharpVariant: Story = {
+  args: {
+    children: 'Shape - Sharp',
+    shape: 'sharp',
+  },
+  name: 'Shape - Sharp',
 };

e6f407494fa918afbf09c7df715450604b908655
Aleksei Pavlov <pavlov.aleksey.m@gmail.com>
2025-10-28 17:25:49 +0100

    [CDX-261] Create Badge (#10)
    * [CDX-261] Add Badge component with Storybook stories and tests
    
    * Remove unnecessary files
    
    * Refactor badge component styles by removing commented lines
    
    * [CDX-261] Update import paths for RenderPropsWrapper in badge and button components

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 // Components
 export { default as Button } from './components/ui/button';
+export { default as Badge } from './components/ui/badge';
 
 // Hooks
 
 // Types
 export type { ButtonVariants, ButtonOverrides, ButtonProps } from './components/ui/button';
+export type { BadgeVariants, BadgeOverrides, BadgeProps } from './components/ui/badge';
 export * from './types';

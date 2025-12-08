a9f5d6887bab3acac50f9b564252fa478e77f090
Enes Kutay SEZEN <eneskutaysezen@gmail.com>
2025-12-08 18:10:40 +0300

    Add guard for SSR

--- src/utils/styleHelpers.ts
+++ src/utils/styleHelpers.ts
@@ -3,10 +3,15 @@ import { twMerge } from 'tailwind-merge';
 
 export function getPreferredColorScheme() {
   let colorScheme = 'light';
+
+  // Guard clause for SSR environments
+  if (typeof window === 'undefined') return colorScheme;
+
   // Check if the dark-mode Media-Query matches
   if (window.matchMedia('(prefers-color-scheme: dark)')?.matches) {
     colorScheme = 'dark';
   }
+
   return colorScheme;
 }
 
